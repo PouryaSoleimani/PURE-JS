10cd78f68142e69d6fa865200ed180a20d2e01d4
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-03 06:33:21 -0700

    events

--- JS/EXERCISE.js
+++ JS/EXERCISE.js
@@ -1,5 +1,5 @@
 // document.getElementById("h12").innerHTML="hello";
-var h3 =document.createElement('h3');
+var h3 = document.createElement('h3');
 h3.classList.add('testText')
 h3.id = 'testText'
-h3.setAttribute('id','testtext')
+h3.setAttribute('id', 'testtext')
--- JS/EXPORTS.js
+++ JS/EXPORTS.js
@@ -1,8 +1,8 @@
 const name = "Jesse";
 const age = 40;
 
- function sayHello() {
-console.log('hello');
+function sayHello() {
+  console.log('hello');
 }
 
-export {name, age, sayHello};
+export { name, age, sayHello };
