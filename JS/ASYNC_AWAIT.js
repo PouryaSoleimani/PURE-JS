4332e7ccae13178b21ca8c33326f45acec373d38
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-03 04:14:22 -0700

    ASYNC AWAIT

--- JS/ASYNC_AWAIT.js
+++ JS/ASYNC_AWAIT.js
@@ -1,21 +1,23 @@
-console.log(`%c ASYNC & AWAIT --------------------------`,`color:yellow`);
+console.log(`%c ASYNC & AWAIT --------------------------`, `color:yellow`);
+console.log(`%c **HELLO WORLD**`, `color : green`)
+console.log(`%c DESIGNED AND DEVELOPED BY TEAM GRAPHIC Co.`, `color : yellow`)
 
-const fetchData= (url,callback)=>{
-     setTimeout(  () => {
-        let data = {name:"pourya"}
-         callback(data)
-    }, 2000);
+const fetchData = (url, callback) => {
+  setTimeout(() => {
+    let data = { name: "pourya" }
+    callback(data)
+  }, 2000);
 }
 // fetchData(`roocket.ir`,(data)=>{ console.log(data) })
 
-const saveDataToDB = (data,callback)=>{
-    setTimeout(() => {
-        callback(true)
-    }, 2000);
+const saveDataToDB = (data, callback) => {
+  setTimeout(() => {
+    callback(true)
+  }, 2000);
 }
 
-fetchData(`roocket.ir`,(data)=>{ 
-   saveDataToDB(data,(status)=>{
+fetchData(`roocket.ir`, (data) => {
+  saveDataToDB(data, (status) => {
     console.log(status);
-   })
+  })
 })
