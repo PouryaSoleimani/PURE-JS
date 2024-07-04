2057fefad356dec304b3dd0271cb1d73cfbc6815
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-04 05:30:21 -0700

    arrays

--- REVIEW.js
+++ REVIEW.js
@@ -1,18 +1,21 @@
+//^ SPREAD SYNTAX
 let me1 = { name: "pourya", age: 30 }
 let me2 = { job: "developer" }
 let fullMe = { ...me1, ...me2 }
 console.log(fullMe)
 
-
+//^ DATE
 let d1 = new Date();
 let fullDate = `${d1.getFullYear()} / ${d1.getMonth() + 1} / ${d1.getDate()}`;
 console.log(fullDate);
 
-
+//^ REDUCE
 function sum(...args) {
   let a = args.reduce((a, b) => { return a + b })
   console.log(a)
 }
 sum(1, 2, 3, 4, 5)
+
+
 
 
