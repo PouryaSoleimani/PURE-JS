ebc06eee98b2ad67226a9c71cf20cb8aeef9ab1f
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-07-04 05:24:40 -0700

    arrays

--- REVIEW.js
+++ REVIEW.js
@@ -1,5 +1,11 @@
-`let me1 = { name: "pourya", age: 30 }
+let me1 = { name: "pourya", age: 30 }
 let me2 = { job: "developer" }
-
 let fullMe = { ...me1, ...me2 }
-console.log(fullMe)`
+console.log(fullMe)
+
+
+let d1 = new Date();
+let fullDate = `${d1.getFullYear()} / ${d1.getMonth() + 1} / ${d1.getDate()}`;
+console.log(fullDate);
+
+
